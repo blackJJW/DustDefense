f79f71ee96a81a70856c9e7253e878315f6f373e
hce2 <codms228@gmail.com>
2022-08-17 09:36:42 +0900

    등록, 삭제, 수정, 공지글, 페이징까지 완성

--- DustDefense/src/main/java/com/example/demo/config/DatabaseConfig.java
+++ DustDefense/src/main/java/com/example/demo/config/DatabaseConfig.java
@@ -1,7 +1,7 @@
 package com.example.demo.config;
 
-import javax.sql.DataSource;
-
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -12,16 +12,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
 
 @Configuration
 @PropertySource("classpath:/application.properties")
 public class DatabaseConfig {
-	@Autowired
-	private ApplicationContext context;
-	
-	@Bean
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public HikariConfig hikariConfig() {
         return new HikariConfig();
@@ -31,12 +31,13 @@ public class DatabaseConfig {
     public DataSource dataSource() {
         return new HikariDataSource(hikariConfig());
     }
-	
-	@Bean
+
+    @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource());
-//		factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mappers/**/*Mapper.xml"));
+		factoryBean.setMapperLocations(context.getResources("classpath:/mappers/**/*Mapper.xml"));
+        factoryBean.setConfiguration(mybatisConfig());
         return factoryBean.getObject();
     }
 
@@ -44,5 +45,11 @@ public class DatabaseConfig {
     public SqlSessionTemplate sqlSession() throws Exception {
         return new SqlSessionTemplate(sqlSessionFactory());
     }
-	
+    
+    @Bean
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration mybatisConfig() {
+        return new org.apache.ibatis.session.Configuration();
+    }
+
 }
