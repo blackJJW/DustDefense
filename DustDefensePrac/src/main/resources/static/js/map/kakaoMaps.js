2a42780a30fd5590ff6ef18c378875bc7c4c3ea4
blackJJW <jjinwoo92@gmail.com>
2022-08-19 01:42:05 +0900

    맵 api 수정중  카카오 행정 구역 구분 스크립트 수정 필요/ openroad api 연구 필요, ajax 연구 필요

--- DustDefensePrac/src/main/resources/static/js/map/kakaoMaps.js
+++ DustDefensePrac/src/main/resources/static/js/map/kakaoMaps.js
@@ -1,152 +1,162 @@
-// 지도 생성
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
+import geojson from "/data/geo/seoulGeo.json" assert { type: "json" };
 
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption); 
 
-//------
+//2022.08.18 문제 : 선택한 폴리곤이 흰색일때만 클릭이됨
+// 클릭이벤트는 하나인데 드래그앤드랍, 포인트찍기, 지도확대, 지역명찍기가 다 적용되고있음
 
-// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-var mapTypeControl = new kakao.maps.MapTypeControl();
 
-// 지도 타입 컨트롤을 지도에 표시합니다
-// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
-map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+const { kakao } = window;
 
-//-----
 
-// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-var zoomControl = new kakao.maps.ZoomControl();
-map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+let data = geojson.features;
+let coordinates = []; //좌표 저장 배열
+let name = ''; //행정구 이름
 
-//-----
+let polygons = [];
 
-//-------------------------------------------------------------------------------
-//----- 지도 이동시키기 --------------------------------------------------------------
-function setCenter() {            
-    // 이동할 위도 경도 위치를 생성합니다 
-    var moveLatLon = new kakao.maps.LatLng(33.452613, 126.570888);
+const mapContainer = document.getElementById('map'); // 지도를 표시할 div
+const mapOption = {
+  center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+  level: 9, // 지도의 확대 레벨
+};
+
+const map = new kakao.maps.Map(mapContainer, mapOption);
+const customOverlay = new kakao.maps.CustomOverlay({});
+
+
+/* 좌표와 이름으로 화면에 표현할 모든 행정구역명 가져오기 */
+const displayArea = (coordinates, name) => {
+  let path = [];
+  let points = [];
+  //let areaResult = pollution.filter((item) => item[0] === name); //없어도 됨
+
+  coordinates[0].forEach((coordinate) => {
+    let point = {};
+    point.x = coordinate[1];
+    point.y = coordinate[0];
+    points.push(point);
+    path.push(new kakao.maps.LatLng(coordinate[1], coordinate[0]));
+  });
+
+  let polygon = new kakao.maps.Polygon({
+    map: map,
+    path: path, // 그려질 다각형의 좌표 배열입니다
+    strokeWeight: 2, // 선의 두께입니다
+    strokeColor: '#004c80', // 선의 색깔입니다
+    strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    strokeStyle: 'solid', // 선의 스타일입니다
+    fillColor: '#fff', // 채우기 색깔입니다
+    fillOpacity: 0.7, // 채우기 불투명도 입니다
+  });
+
+  polygons.push(polygon);
+
+
+  let prevName = "";
+
+
+  // 다각형에 mouseover 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 변경합니다
+  // 지역명을 표시하는 커스텀오버레이를 지도위에 표시합니다
+  kakao.maps.event.addListener(polygon, 'mouseover', function (mouseEvent) {
+    //방금선택한 위치의 지역구이름이 이미선택된 지역구이름과 같으면 아래 행위 안하도록
     
-    // 지도 중심을 이동 시킵니다
-    map.setCenter(moveLatLon);
+    console.log("prevName : "+prevName);
+    console.log("name : "+name);
+    
+
+    //오버레이 형태 html로 구성
+    customOverlay.setContent('<div class="area" pointer-events: none; >' + name + '</div>');
+
+    customOverlay.setPosition(mouseEvent.latLng);//클릭했을때 마우스의 위치를 커스텀오버레이의 시작점으로 설정
+    customOverlay.setMap(map); //오버레이드를 맵에 세팅
+
+    if(prevName == name){
+        return;
+    }else{
+        polygon.setOptions({ fillColor: '#09f' });  //폴리곤 색깔
+        prevName = name;
+    }
+    
+    
+  });
+
+  // 다각형에 mousemove 이벤트를 등록하고 이벤트가 발생하면 커스텀 오버레이의 위치를 변경합니다
+  kakao.maps.event.addListener(polygon, 'mousemove', function (mouseEvent) {
+    customOverlay.setPosition(mouseEvent.latLng);
+  });
+
+  // 다각형에 mouseout 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 원래색으로 변경합니다
+  // 커스텀 오버레이를 지도에서 제거합니다
+  kakao.maps.event.addListener(polygon, 'mouseout', function () {
+    polygon.setOptions({ fillColor: '#fff' });
+    customOverlay.setMap(null);
+  });
+   
+  // 다각형에 click 이벤트를 등록하고 이벤트가 발생하면 해당 지역 확대를 확대   
+  kakao.maps.event.addListener(polygon, 'click', function() {
+    console.log("폴리곤이 이벤트 인식함");
+    // 현재 지도 레벨에서 2레벨 확대한 레벨
+    let level = map.getLevel() - 2;
+    
+    // 지도를 클릭된 폴리곤의 중앙 위치를 기준으로 확대
+    map.setLevel(level, {anchor: centroid(points), animate:{
+        duration: 350  // 확대 애니메이션 시간
+    }});
+    deletePolygon(polygons);
+   
+  });
+ };
+
+
+/* data는 gson의 feature*/
+data.forEach((val) => {
+  coordinates = val.geometry.coordinates;
+  name = val.properties.SIG_KOR_NM;
+
+  displayArea(coordinates, name);
+});
+
+function centroid(points){
+   let i, j, len, p1, p2, f, area, x, y;
+   
+   area = x = y = 0;
+   
+   for(i = 0, len = points.length, j = len - 1; i < len; j = i++){
+      p1 = points[i];
+      p2 = points[j];
+      
+      f = p1.y * p2.x - p2.y * p1.x;
+      x += (p1.x + p2.x) * f;
+      y += (p1.y + p2.y) * f;
+      area += f * 3;
+   }
+   return new kakao.maps.LatLng(x / area, y / area);
 }
 
-function panTo() {
-    // 이동할 위도 경도 위치를 생성합니다 
-    var moveLatLon = new kakao.maps.LatLng(33.450580, 126.574942);
-    
-    // 지도 중심을 부드럽게 이동시킵니다
-    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
-    map.panTo(moveLatLon);            
+function deletePolygon(polygons){
+   for(let i = 0; i < polygons.length; i++){
+      polygons[i].setMap(null);
+   }
+   polygons = [];
 }
-//----------------------------------------------------------------------------------
-//----- 지도 레벨 바꾸기 ---------------------------------------------------------------
-// 지도 레벨을 표시합니다
-displayLevel();
- 
-// 지도 레벨은 지도의 확대 수준을 의미합니다
-// 지도 레벨은 1부터 14레벨이 있으며 숫자가 작을수록 지도 확대 수준이 높습니다
-function zoomIn() {        
-    // 현재 지도의 레벨을 얻어옵니다
-    var level = map.getLevel();
-    
-    // 지도를 1레벨 내립니다 (지도가 확대됩니다)
-    map.setLevel(level - 1);
-    
-    // 지도 레벨을 표시합니다
-    displayLevel();
-}    
+//---------------
 
-function zoomOut() {    
-    // 현재 지도의 레벨을 얻어옵니다
-    var level = map.getLevel(); 
-    
-    // 지도를 1레벨 올립니다 (지도가 축소됩니다)
-    map.setLevel(level + 1);
-    
-    // 지도 레벨을 표시합니다
-    displayLevel(); 
-}    
- 
-function displayLevel(){
-    var levelEl = document.getElementById('maplevel');
-    levelEl.innerHTML = '현재 지도 레벨은 ' + map.getLevel() + ' 레벨 입니다.';
-}
-//---------------------------------------------------------------------------------
-//----- 지도 정보 얻어오기 -------------------------------------------------------------
-
-function getInfo() {
-    // 지도의 현재 중심좌표를 얻어옵니다 
-    var center = map.getCenter(); 
-    
-    // 지도의 현재 레벨을 얻어옵니다
-    var level = map.getLevel();
-    
-    // 지도타입을 얻어옵니다
-    var mapTypeId = map.getMapTypeId(); 
-    
-    // 지도의 현재 영역을 얻어옵니다 
-    var bounds = map.getBounds();
-    
-    // 영역의 남서쪽 좌표를 얻어옵니다 
-    var swLatLng = bounds.getSouthWest(); 
-    
-    // 영역의 북동쪽 좌표를 얻어옵니다 
-    var neLatLng = bounds.getNorthEast(); 
-    
-    // 영역정보를 문자열로 얻어옵니다. ((남,서), (북,동)) 형식입니다
-    var boundsStr = bounds.toString();
-    
-    
-    var message = '지도 중심좌표는 위도 ' + center.getLat() + ', <br>';
-    message += '경도 ' + center.getLng() + ' 이고 <br>';
-    message += '지도 레벨은 ' + level + ' 입니다 <br> <br>';
-    message += '지도 타입은 ' + mapTypeId + ' 이고 <br> ';
-    message += '지도의 남서쪽 좌표는 ' + swLatLng.getLat() + ', ' + swLatLng.getLng() + ' 이고 <br>';
-    message += '북동쪽 좌표는 ' + neLatLng.getLat() + ', ' + neLatLng.getLng() + ' 입니다';
-    
-    console.log(message);
-    // 개발자도구를 통해 직접 message 내용을 확인해 보세요.
-    // ex) console.log(message);
-}
-//--------------------------------------------------------------------------------------------------
-//----- 교통정보 가져오기 -------------------------------------------------------------------
-function turnOffTrafficInfo(){
-
-	// 아래 코드는 위에서 추가한 교통정보 지도타입을 제거합니다
-	map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);    
-}
-
-function turnOnTrafficInfo(){
-
-	// 지도에 교통정보를 표시하도록 지도타입을 추가합니다
-	map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);    
-}
-//------------------------------------------------------------------------------------------
-//------ 클릭 이벤트 등록--------------------------------------------------------------------------
+//2022.08.18 배성원. 마커 안쓸거기때문에 주석처리
 // 지도를 클릭한 위치에 표출할 마커입니다
-var marker = new kakao.maps.Marker({ 
-    // 지도 중심좌표에 마커를 생성합니다 
-    position: map.getCenter() 
-}); 
-// 지도에 마커를 표시합니다
-marker.setMap(map);
+// var marker = new kakao.maps.Marker({ 
+//     // 지도 중심좌표에 마커를 생성합니다 
+//     position: map.getCenter() 
+// }); 
+// // 지도에 마커를 표시합니다
+// marker.setMap(map)
 
-
-
-// 지도에 클릭 이벤트를 등록합니다
-// 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
 kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
-    
+    console.log("맵이 클릭 인식함");
     // 클릭한 위도, 경도 정보를 가져옵니다 
     var latlng = mouseEvent.latLng; 
     
     // 마커 위치를 클릭한 위치로 옮깁니다
-    marker.setPosition(latlng);
+    //marker.setPosition(latlng);
     
     var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
     message += '경도는 ' + latlng.getLng() + ' 입니다';
@@ -155,66 +165,34 @@ kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
     resultDiv.innerHTML = message;
     
 });
-//-----------------------------------------------------------------------------------
-//----- 이동 이벤트 등록 -----------------------------------------------------------------
-// 마우스 드래그로 지도 이동이 완료되었을 때 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
-kakao.maps.event.addListener(map, 'dragend', function() {        
-    
-    // 지도 중심좌표를 얻어옵니다 
-    var latlng = map.getCenter(); 
-    
-    var message = '변경된 지도 중심좌표는 ' + latlng.getLat() + ' 이고, ';
-    message += '경도는 ' + latlng.getLng() + ' 입니다';
-    
-    var resultDiv = document.getElementById('result');  
-    resultDiv.innerHTML = message;
-    
-});
 
-//--------------------------------------------------------------------------------------------
-//------ 지역 별 폴리곤 ---------------------------------------------------------------------------
-// 행정구역 구분
-$.getJSON("/resources/json/geoJson/siDo.json", function(geojson){
-	console.log("get done.");
-	var data = geojson.features;
-	var coordinates = [];  // 좌표 저장할 배열
-	var name = '';   // 행정 이름
-	
-	$.each(data, function(index, val){
-		
-		coordinates = val.geometry.coordinates;
-		name = val.properties.CTP_KOR_NM;
-		
-		displayArea(coordinates, name);
-	})
-})
+//---------------
+let pathData;
 
-var polygons = []; // function 안 쪽에 지역변수로 넣으니깐 폴리곤 하나 생성할 때마다 배열이 비어서 클릭했을 때 전체를 못 없애줌.그래서 전역 변수로 만듦
+var request = new XMLHttpRequest();
 
-// 행정구역 폴리곤
-function displayArea(coordinates, name){
-	var path = [];	 // 폴리곤 그려줄 path
-	var points = []; // 중심 좌표 구하기 위한 지역구 좌표들
-	
-	$.each(coordinates[0], function(index, coordinate){
-		console.log(coordinates);
-		
-		var point = new Object();
-		point.x = coordinate[1];
-		point.y = coordinate[0];
-		points.push(point);
-		path.push(new daum.maps.LatLng(coordinate[1], coordinate[0]));
-	})
-	
-	// 다각형 생성
-	var polygon = new kakao.maps.Polygon({
-	    path:path, // 그려질 다각형의 좌표 배열입니다
-	    strokeWeight: 3, // 선의 두께입니다
-	    strokeColor: '#39DE2A', // 선의 색깔입니다
-	    strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-	    strokeStyle: 'longdash', // 선의 스타일입니다
-	    fillColor: '#A2FF99', // 채우기 색깔입니다
-	    fillOpacity: 0.7 // 채우기 불투명도 입니다
-	});
-	polygon.setMap(map);
+request.open('GET', 'https://api.openrouteservice.org/v2/directions/cycling-regular?api_key=5b3ce3597851110001cf624888ae0402478a4e078fb90dfac4b683ab&start=8.681495,49.41461&end=8.687872,49.420318');
+
+request.setRequestHeader('Accept', 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8');
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+  }
+};
+
+request.send();
+
+request.onload = () =>{
+	if(request.status === 200){
+		pathData = JSON.parse(request.responseText);
+		console.log(pathData);
+		console.log(pathData.bbox);
+		console.log(pathData.features[0].geometry.coordinates);
+	} else {
+		console.log('Error', request.status, request.statusText);
+	}
 }
+
